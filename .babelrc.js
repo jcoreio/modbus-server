52446be69ba097296572f4023ca6c71a4daf9e14
Jason McLaurin <jrmclaurin@users.noreply.github.com>
2021-06-10 17:12:38 -0500

    chore: get rid of unneeded @babel/proposal-class-properties and proposal-object-rest-spread @babel/preset-env now applies those transformations when needed for the target runtime

--- .babelrc.js
+++ .babelrc.js
@@ -1,19 +1,6 @@
 module.exports = function(api) {
-  const plugins = [
-    '@babel/proposal-class-properties',
-    '@babel/proposal-object-rest-spread',
-  ]
-  const presets = [
-    ['@babel/env', { targets: { node: '8' } }],
-    '@babel/typescript',
-  ]
-
-  if (api.env(['test', 'coverage'])) {
-    plugins.push('@babel/transform-runtime')
+  return {
+    plugins: api.env('coverage') ? ['babel-plugin-istanbul'] : [],
+    presets: [['@babel/env', { targets: { node: '8' } }], '@babel/typescript'],
   }
-  if (api.env('coverage')) {
-    plugins.push('babel-plugin-istanbul')
-  }
-
-  return { plugins, presets }
 }
